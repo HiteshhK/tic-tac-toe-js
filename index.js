173fef76c18206bdbf6ec09daae987c93fc39440
HiteshhK <kaushikhitesh002@gmail.com>
2022-01-26 22:20:38 +0530

    1 file modified minmax-algorithm

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function emptySquare() {
 }
 
 function bestSpot() {
-  return emptySquare()[0];
+  return minimax(origBoard, aiPlayer).index;
 }
 
 function checkTie() {
@@ -98,4 +98,53 @@ function checkTie() {
     return true;
   }
   return false;
+}
+function minimax(newBoard, player) {
+  var availSpots = emptySquare(newBoard);
+
+  if (checkWin(newBoard, huPlayer)) {
+    return { score: -10 };
+  } else if (checkWin(newBoard, aiPlayer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
+
+  var moves = [];
+  for (let i = 0; i < availSpots.length; i++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+
+    if (player === aiPlayer) move.score = minimax(newBoard, huPlayer).score;
+    else move.score = minimax(newBoard, aiPlayer).score;
+    newBoard[availSpots[i]] = move.index;
+    if (
+      (player === aiPlayer && move.score === 10) ||
+      (player === huPlayer && move.score === -10)
+    )
+      return move;
+    else moves.push(move);
+  }
+
+  let bestMove, bestScore;
+  if (player === aiPlayer) {
+    bestScore = -1000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    bestScore = 1000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
 }
